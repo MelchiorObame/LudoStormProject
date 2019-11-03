42abaf25a7e494e795810754b8eec827e39b14a0
Melchior Obame <obamemelchior@gmail.com>
2019-11-04 00:19:22 +0100

    Project

---
+++ My Cedar Point  Project/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lbAge = new System.Windows.Forms.Label();
+            this.lblSeason = new System.Windows.Forms.Label();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtSeason = new System.Windows.Forms.TextBox();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.picAmusementPark = new System.Windows.Forms.PictureBox();
+            this.picSki = new System.Windows.Forms.PictureBox();
+            this.picClosed = new System.Windows.Forms.PictureBox();
+            this.picWaterPark = new System.Windows.Forms.PictureBox();
+            this.btnRecommend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picStormLand = new System.Windows.Forms.PictureBox();
+            this.lblRide = new System.Windows.Forms.Label();
+            this.lblPark = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picAmusementPark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClosed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWaterPark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStormLand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(22, 47);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lbAge
+            // 
+            this.lbAge.AutoSize = true;
+            this.lbAge.Location = new System.Drawing.Point(26, 156);
+            this.lbAge.Name = "lbAge";
+            this.lbAge.Size = new System.Drawing.Size(42, 20);
+            this.lbAge.TabIndex = 1;
+            this.lbAge.Text = "Age ";
+            // 
+            // lblSeason
+            // 
+            this.lblSeason.AutoSize = true;
+            this.lblSeason.Location = new System.Drawing.Point(26, 276);
+            this.lblSeason.Name = "lblSeason";
+            this.lblSeason.Size = new System.Drawing.Size(64, 20);
+            this.lblSeason.TabIndex = 2;
+            this.lblSeason.Text = "Season (Spring, Summer, Fall or Winter)";
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Location = new System.Drawing.Point(26, 397);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(100, 20);
+            this.lblTemperature.TabIndex = 3;
+            this.lblTemperature.Text = "Temperature";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(26, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 26);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(26, 189);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(200, 26);
+            this.txtAge.TabIndex = 5;
+            // 
+            // txtSeason
+            // 
+            this.txtSeason.Location = new System.Drawing.Point(26, 299);
+            this.txtSeason.Name = "txtSeason";
+            this.txtSeason.Size = new System.Drawing.Size(196, 26);
+            this.txtSeason.TabIndex = 6;
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(30, 420);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(192, 26);
+            this.txtTemperature.TabIndex = 7;
+            // 
+            // picAmusementPark
+            // 
+            this.picAmusementPark.Image = ((System.Drawing.Image)(resources.GetObject("picAmusementPark.Image")));
+            this.picAmusementPark.Location = new System.Drawing.Point(420, 85);
+            this.picAmusementPark.Name = "picAmusementPark";
+            this.picAmusementPark.Size = new System.Drawing.Size(300, 300);
+            this.picAmusementPark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAmusementPark.TabIndex = 8;
+            this.picAmusementPark.TabStop = false;
+            this.picAmusementPark.Visible = false;
+            // 
+            // picSki
+            // 
+            this.picSki.Image = ((System.Drawing.Image)(resources.GetObject("picSki.Image")));
+            this.picSki.Location = new System.Drawing.Point(420, 85);
+            this.picSki.Name = "picSki";
+            this.picSki.Size = new System.Drawing.Size(300, 300);
+            this.picSki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSki.TabIndex = 9;
+            this.picSki.TabStop = false;
+            this.picSki.Visible = false;
+            // 
+            // picClosed
+            // 
+            this.picClosed.Image = ((System.Drawing.Image)(resources.GetObject("picClosed.Image")));
+            this.picClosed.Location = new System.Drawing.Point(420, 85);
+            this.picClosed.Name = "picClosed";
+            this.picClosed.Size = new System.Drawing.Size(300, 300);
+            this.picClosed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picClosed.TabIndex = 10;
+            this.picClosed.TabStop = false;
+            this.picClosed.Visible = false;
+            // 
+            // picStormLand
+            // 
+            this.picStormLand.Image = ((System.Drawing.Image)(resources.GetObject("picStormLand.Image")));
+            this.picStormLand.Location = new System.Drawing.Point(420, 85);
+            this.picStormLand.Name = "picStormLand";
+            this.picStormLand.Size = new System.Drawing.Size(300, 250);
+            this.picStormLand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStormLand.TabIndex = 15;
+            this.picStormLand.TabStop = false;
+            // 
+            // picWaterPark
+            // 
+            this.picWaterPark.Image = ((System.Drawing.Image)(resources.GetObject("picWaterPark.Image")));
+            this.picWaterPark.Location = new System.Drawing.Point(420, 85);
+            this.picWaterPark.Name = "picWaterPark";
+            this.picWaterPark.Size = new System.Drawing.Size(300, 300);
+            this.picWaterPark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWaterPark.TabIndex = 11;
+            this.picWaterPark.TabStop = false;
+            this.picWaterPark.Visible = false;
+
+            // 
+            // btnRecommend
+            // 
+            this.btnRecommend.Location = new System.Drawing.Point(30, 496);
+            this.btnRecommend.Name = "btnRecommend";
+            this.btnRecommend.Size = new System.Drawing.Size(136, 36);
+            this.btnRecommend.TabIndex = 12;
+            this.btnRecommend.Text = "Recommend";
+            this.btnRecommend.UseVisualStyleBackColor = true;
+            this.btnRecommend.Click += new System.EventHandler(this.btnRecommend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(300, 496);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(577, 496);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 36);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblRide
+            // 
+            this.lblRide.AutoSize = true;
+            this.lblRide.Location = new System.Drawing.Point(470, 440);
+            this.lblRide.Name = "lblRide";
+            this.lblRide.Size = new System.Drawing.Size(40, 20);
+            this.lblRide.TabIndex = 16;
+            this.lblRide.ForeColor = System.Drawing.Color.Red;
+            this.lblRide.Font = new Font("Verdana", 15);
+            // 
+            // lblPark
+            // 
+            this.lblPark.AutoSize = true;
+            this.lblPark.Location = new System.Drawing.Point(470, 400);
+            this.lblPark.Name = "lblPark";
+            this.lblPark.Size = new System.Drawing.Size(40, 20);
+            this.lblPark.TabIndex = 17;
+            this.lblPark.ForeColor = System.Drawing.Color.Blue;
+            this.lblPark.Font = new Font("Verdana", 15);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 550);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Controls.Add(this.lblPark);
+            this.Controls.Add(this.lblRide);
+            this.Controls.Add(this.picStormLand);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRecommend);
+            this.Controls.Add(this.picWaterPark);
+            this.Controls.Add(this.picClosed);
+            this.Controls.Add(this.picSki);
+            this.Controls.Add(this.picAmusementPark);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.txtSeason);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTemperature);
+            this.Controls.Add(this.lblSeason);
+            this.Controls.Add(this.lbAge);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "StormLand";
+            ((System.ComponentModel.ISupportInitialize)(this.picAmusementPark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClosed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWaterPark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStormLand)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lbAge;
+        private System.Windows.Forms.Label lblSeason;
+        private System.Windows.Forms.Label lblTemperature;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtSeason;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.PictureBox picAmusementPark;
+        private System.Windows.Forms.PictureBox picSki;
+        private System.Windows.Forms.PictureBox picClosed;
+        private System.Windows.Forms.PictureBox picWaterPark;
+        private System.Windows.Forms.Button btnRecommend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picStormLand;
+        private System.Windows.Forms.Label lblRide;
+        private System.Windows.Forms.Label lblPark;
+    }
+}
+
---
+++ My Cedar Point  Project/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+    Age 
+    under 7 ( 0-7) 
+        Rain Storm
+        Lil’ Tornado
+        Snow Drift Slope
+    -7-13 
+      Summer Storm
+      Storm Chaser
+      Blizzard Slope
+    > 7 
+      Thunder Storm
+      Storm Buster
+      Avalanche Slope
+    
+    Temperature
+        Water Park (>=74 && summer oonly)
+        Amusement Park (for spring, Summer and fall)
+        Storm, Buster Ski Park, Avalanche  ( only during winter) 
+ */
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecommend_Click(object sender, EventArgs e)
+        {
+            //local Variables declaration
+            String strName;             // Will be the  user name 
+            String strSeason ;          // desired Season
+            String StrPark ;            // what is going to be  the selected Park 
+            String strAttraction;       // what is going to be your ride 
+            int intAge=-1;              // default age
+            int intTemperature;         // the actual temperature 
+            //flags
+            Boolean blnTooWarm = false; // temperature too warm
+            Boolean blnIsError = false; // in case of error
+
+            //Assignments
+            strName = txtName.Text;
+            strSeason = txtSeason.Text;
+            StrPark = "";
+            strAttraction = "";
+
+            ///////////////////////////////////////  CLEAN DATA  /////////////////////////////////////////
+
+            //Remove blank before and after the name
+            strName = strName.Trim();
+
+            //checking age label
+            try
+            {
+                intAge = Int32.Parse(txtAge.Text);
+            }
+            catch (FormatException)
+            {
+                blnIsError = true;
+                Console.WriteLine($"Unable to parse age '{txtAge.Text}' to Integer");
+                MessageBox.Show("please enter a number for your age");
+            }
+
+            //checking season label
+            strSeason = strSeason.Trim().ToLower();
+            if (!strSeason.Equals("spring") && !strSeason.Equals("summer") && !strSeason.Equals("fall") && !strSeason.Equals("winter"))
+            {
+                blnIsError = true;
+                Console.WriteLine("please input correct season");
+                MessageBox.Show("you did not enter a valid season");
+            }
+
+            //checking temperature label
+            if (!int.TryParse(txtTemperature.Text, out intTemperature))
+            {
+                blnIsError = true;
+                Console.WriteLine($"Unable to parse temprature '{txtTemperature.Text}' to Integer");
+                MessageBox.Show("Please enter a number for your temperature");
+            }
+
+            ///////////////////////////////////  BEGIN IF STRUCTURE  /////////////////////////////////////
+            //Only during winter when <7 Snow Drift Slope, 7-13 Blizzard Slope, >=14 Avalanche Slope 
+            if (!blnIsError)
+            {
+                String season = strSeason.Substring(0, 2);
+                if (season.Equals("wi"))
+                {
+                    //Nested if/elseIf/Else
+                    if (intTemperature <= 50)
+                    {
+                        picClosed.Visible = false;
+                        picStormLand.Visible = false;
+                        picAmusementPark.Visible = false;
+                        picWaterPark.Visible = false;
+                        picSki.Visible = true;
+                        StrPark = "Ski Park";
+                        if (intAge < 7)
+                            strAttraction = "Snow Drift Slope";
+                        else if (intAge >= 7 && intAge <= 13)
+                            strAttraction = "Blizzard Slope";
+                        else
+                            strAttraction = "Avalanche Slope";
+                    }
+                    else
+                    {
+                        blnTooWarm = true;
+                    }
+                }
+                else
+                {
+                    //Spring, Fall and  Summmer with temparature under 74 dregree
+                    if (season.Equals("sp") || season.Equals("fa") || (season.Equals("su") && (intTemperature < 74)))
+                    {
+                        //with a switch selection
+                        StrPark = "Amusement Park";
+                        switch (intAge)
+                        {
+                            case 0:case 1: case 2: case 3:case 4:case 5:case 6:
+                                strAttraction = "Lil' Tornado";
+                                break;
+                            case 7:case 8: case 9: case 10: case 11: case 12: case 13 :
+                                strAttraction = "Storm Chaser";
+                                break;
+                            default:
+                                strAttraction = "Storm Buster";
+                                break;
+                        }
+                        picClosed.Visible = false;
+                        picStormLand.Visible = false;
+                        picSki.Visible = false;
+                        picWaterPark.Visible = false;
+                        picAmusementPark.Visible = true;
+
+                    }
+                    //ONLY during the summer, <7 Rain Storm, 7-13 Summmer Storm, >= 14 Thunder Storm
+                    else if (season.Equals("su") && (intTemperature >= 74))
+                    {
+                        StrPark = "Water Park";
+                        picClosed.Visible = false;
+                        picStormLand.Visible = false;
+                        picSki.Visible = false;
+                        picWaterPark.Visible = true;
+                        picAmusementPark.Visible = false;
+
+                        //Nested if 
+                        if (intAge < 7)
+                            strAttraction = "Rain Storm";
+                        else if (intAge >= 7 && intAge <= 13)
+                            strAttraction = "Summer Storm";
+                        else
+                            strAttraction = "Thunder Storm";
+                    }
+                }
+              
+                //else
+                //    MessageBox.Show("Let's have fun!!");
+
+                /////////////////////////////////////  DISPLAY OUTPUT  ///////////////////////////////////////
+                // if temperature is >50 in winter
+                if (blnTooWarm)
+                {
+                    picWaterPark.Visible = false;
+                    picAmusementPark.Visible = false;
+                    picSki.Visible = false;
+                    picStormLand.Visible = false;
+                    picClosed.Visible = true;
+                    lblRide.Visible = false;
+                    lblPark.Visible = false;
+                    MessageBox.Show(strName + " because it's " + intTemperature + " this Winter" + " Our ski Park is closed because it is too warm.");
+                    picClosed.Visible = true;
+                }
+                else
+                {
+                    lblPark.Visible = true;
+                    lblRide.Visible = true;
+
+                    lblRide.Text = strAttraction;
+                    lblPark.Text = StrPark;
+                    if (strName.Equals(""))
+                        strName ="Customer";
+                    //capitalize first char of season
+                    strSeason= System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(strSeason.ToLower());
+                    MessageBox.Show("Hello " + strName + " ,because you are " + intAge + " years old, and  it is " + strSeason + " you should ride the " + strAttraction + " at the " + StrPark);
+                }
+            }  
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear data
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtTemperature.Text = "";
+            txtSeason.Text = "";
+            lblRide.Visible = false;
+            lblPark.Visible = false;
+            picClosed.Visible = false;
+            picStormLand.Visible = true;
+            picSki.Visible = false;
+            picWaterPark.Visible = false;
+            picAmusementPark.Visible = false;
+
+            //Put the focus back 
+            txtName.Focus();
+        }
+    }
+
+}
+
+
